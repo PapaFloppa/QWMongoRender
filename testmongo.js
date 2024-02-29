b9ff7916c126463d739682e9be626f5df27e5779
PapaFloppa <157617781+PapaFloppa@users.noreply.github.com>
2024-02-29 19:09:24 +0000

    changed to work with my DB

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://user:mypassword@qwittmongodb.6txkswx.mongodb.net/?retryWrites=true&w=majority&appName=QWittMongoDB";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -29,19 +29,19 @@ app.get('/say/:name', function(req, res) {
 
 // Route to access database:
 app.get('/api/mongo/:item', function(req, res) {
-const client = new MongoClient(uri);
-const searchKey = "{ partID: '" + req.params.item + "' }";
-console.log("Looking for: " + searchKey);
+  const client = new MongoClient(uri);
+  const searchKey = "{ Part: '" + req.params.item + "' }";
+  console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('QWittMongoDB');
+    const parts = database.collection('MyThings');
 
     // Hardwired Query for a part that has partID '12345'
-    // const query = { partID: '12345' };
+    //const query = { Part: '10MM Socket' };
     // But we will use the parameter provided with the route
-    const query = { partID: req.params.item };
+    const query = { Part: req.params.item };
 
     const part = await parts.findOne(query);
     console.log(part);
